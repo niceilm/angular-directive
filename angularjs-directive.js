7f406922f4538c8507074f6a1b44a8c606090bd2
niceilm <niceilm@naver.com>
2013-10-26 23:15:45 +0900

    init files

---
+++ angularjs-directive.js
@@ -0,0 +1,60 @@
+/*
+ * angularjs-directive v0.2.0
+ * (c) 2013 niceilm http://niceilm.net
+ * License: MIT
+ */
+'use strict';
+
+angular.module('nangularjs.directive', []).
+    directive('loadingComplete', [function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                if (attr.loadingComplete == 'hide') {
+                    element.hide();
+                } else {
+                    element.show();
+                }
+            }
+        }
+    }]).
+    directive('stopDefault', [function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                var eventType = attr.stopDefault || 'click';
+                element.bind(eventType, function (event) {
+                    event.preventDefault();
+                });
+            }
+        }
+    }]).
+    directive('tpFocus', [function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                var el = element.get(0);
+                try {
+                    if (el && typeof el.focus == "function") {
+                        el.focus();
+                    }
+                } catch (me) {
+                }
+            }
+        }
+    }]).
+    directive('tpKeydownThanBlur', [function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                element.keydown(function ($event) {
+                    if ($event.keyCode !== 13) {
+                        return;
+                    }
+                    $event.preventDefault();
+                    $event.stopPropagation();
+                    $event.target.blur();
+                });
+            }
+        }
+    }]);
