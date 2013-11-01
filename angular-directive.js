8164a7429c1a10937458501f5a74f4aa7b7092e0
niceilm <niceilm@naver.com>
2013-11-01 19:34:05 +0900

    change prefix

--- angular-directive.js
+++ angular-directive.js
@@ -6,55 +6,55 @@
 'use strict';
 
 angular.module('nangular.directive', []).
-    directive('loadingComplete', [function () {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attr) {
-                if (attr.loadingComplete == 'hide') {
-                    element.hide();
-                } else {
-                    element.show();
-                }
-            }
+  directive('naLoadingComplete', [function() {
+    return {
+      restrict : 'A',
+      link : function(scope, element, attr) {
+        if(attr.loadingComplete == 'hide') {
+          element.hide();
+        } else {
+          element.show();
         }
-    }]).
-    directive('stopDefault', [function () {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attr) {
-                var eventType = attr.stopDefault || 'click';
-                element.bind(eventType, function (event) {
-                    event.preventDefault();
-                });
-            }
+      }
+    }
+  }]).
+  directive('naStopDefault', [function() {
+    return {
+      restrict : 'A',
+      link : function(scope, element, attr) {
+        var eventType = attr.stopDefault || 'click';
+        element.bind(eventType, function(event) {
+          event.preventDefault();
+        });
+      }
+    }
+  }]).
+  directive('naFocus', [function() {
+    return {
+      restrict : 'A',
+      link : function(scope, element, attr) {
+        var el = element.get(0);
+        try {
+          if(el && typeof el.focus == "function") {
+            el.focus();
+          }
+        } catch(me) {
         }
-    }]).
-    directive('tpFocus', [function () {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attr) {
-                var el = element.get(0);
-                try {
-                    if (el && typeof el.focus == "function") {
-                        el.focus();
-                    }
-                } catch (me) {
-                }
-            }
-        }
-    }]).
-    directive('tpKeydownThanBlur', [function () {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attr) {
-                element.keydown(function ($event) {
-                    if ($event.keyCode !== 13) {
-                        return;
-                    }
-                    $event.preventDefault();
-                    $event.stopPropagation();
-                    $event.target.blur();
-                });
-            }
-        }
-    }]);
+      }
+    }
+  }]).
+  directive('naKeydownThanBlur', [function() {
+    return {
+      restrict : 'A',
+      link : function(scope, element, attr) {
+        element.keydown(function($event) {
+          if($event.keyCode !== 13) {
+            return;
+          }
+          $event.preventDefault();
+          $event.stopPropagation();
+          $event.target.blur();
+        });
+      }
+    }
+  }]);
