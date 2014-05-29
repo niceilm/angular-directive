4e6cf636363a67bab30ad3ab3cd2c989aa366b3b
parker <parker@kakao.com>
2014-05-29 18:38:27 +0900

    naLinkByLocation 추가

--- angular-directive.js
+++ angular-directive.js
@@ -8,8 +8,8 @@
 angular.module('nangular.directive', []).
   directive('naLoadingComplete', [function() {
     return {
-      restrict : 'A',
-      link : function(scope, element, attr) {
+      restrict: 'A',
+      link: function(scope, element, attr) {
         if(attr.loadingComplete == 'hide') {
           element.hide();
         } else {
@@ -20,8 +20,8 @@ angular.module('nangular.directive', []).
   }]).
   directive('naStopDefault', [function() {
     return {
-      restrict : 'A',
-      link : function(scope, element, attr) {
+      restrict: 'A',
+      link: function(scope, element, attr) {
         var eventType = attr.stopDefault || 'click';
         element.bind(eventType, function(event) {
           event.preventDefault();
@@ -31,8 +31,8 @@ angular.module('nangular.directive', []).
   }]).
   directive('naFocus', ['$timeout', function($timeout) {
     return {
-      restrict : 'A',
-      link : function(scope, element, attr) {
+      restrict: 'A',
+      link: function(scope, element, attr) {
         var el = element.get(0);
         try {
           if(el && typeof el.focus == "function") {
@@ -50,8 +50,8 @@ angular.module('nangular.directive', []).
   }]).
   directive('naWhenActive', ['$location', function($location) {
     return {
-      restrict : 'A',
-      link : function(scope, element, attrs) {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
         if($location.path() === attrs.naWhenActive) {
           element.addClass("active");
         } else {
@@ -63,8 +63,8 @@ angular.module('nangular.directive', []).
   }]).
   directive('naRedirect', function($window, $location) {
     return {
-      restrict : 'A',
-      link : function postLink(scope, element, attrs) {
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {
         if(attrs.naRedirect) {
           var currentUrl = $location.url();
           $window.location.href = attrs.naRedirect + currentUrl;
@@ -72,10 +72,27 @@ angular.module('nangular.directive', []).
       }
     };
   }).
+  directive('naLinkByLocation', function($window) {
+    return {
+      scope: {remotes: "@"},
+      restrict: 'A',
+      link: function postLink(scope, element, attrs) {
+        //  Mobile Safari in standalone mode
+        if(("standalone" in $window.navigator) && $window.navigator.standalone) {
+          element.click(function(event) {
+            if('href' in attrs && attrs.href.indexOf('http') !== -1 && (attrs.href.indexOf($window.location.host) !== -1 || scope.remotes)) {
+              event.preventDefault();
+              $window.location.href = attrs.href;
+            }
+          });
+        }
+      }
+    };
+  }).
   directive('naKeydownThanBlur', [function() {
     return {
-      restrict : 'A',
-      link : function(scope, element, attr) {
+      restrict: 'A',
+      link: function(scope, element, attr) {
         element.keydown(function($event) {
           if($event.keyCode !== 13) {
             return;
